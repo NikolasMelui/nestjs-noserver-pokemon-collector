f081b3b4989f98c9d5cf72dd5a9d309f4f5ab932
nikolasmelui <slak@samaradom.ru>
2021-06-08 00:20:51 +0300

    Add new getPokemons method to DownloaderService

--- src/downloader/downloader.service.ts
+++ src/downloader/downloader.service.ts
@@ -1,13 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
 
+const URL = 'https://pokeapi.co/api/v2/pokemon';
+
 @Injectable()
 export class DownloaderService {
+  async getPokemons(): Promise<string> {
+    try {
+      const result = await axios.get(`${URL}?limit=10000`);
+      return result?.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   async getPokemon(name: string): Promise<string> {
     try {
-      const URL = 'https://pokeapi.co/api/v2/pokemon';
       const result = await axios.get(`${URL}/${name}`);
-      return result.data;
+      return result?.data;
     } catch (error) {
       console.error(error);
     }
