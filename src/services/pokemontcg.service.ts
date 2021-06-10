0b6d3d0777833f6041406df91403f004df6221b2
nikolasmelui <slak@samaradom.ru>
2021-06-10 22:56:32 +0300

    Add PokemonTcgService

---
+++ src/services/pokemontcg.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import axios from 'axios';
+
+import configuration from '../config/configuration';
+
+@Injectable()
+export class PokemonTcgService {
+  private config: Record<string, unknown>;
+
+  constructor() {
+    this.config = configuration().pokemon_tcg;
+  }
+
+  async getPokemons(): Promise<any> {
+    try {
+      const result = await axios.get(`${this.config.URL}/cards`, {
+        headers: {
+          'X-Api-Key': this.config.API_KEY,
+        },
+      });
+      return result.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async getPokemon(name: string): Promise<any> {
+    try {
+      const result = await axios.get(
+        `${this.config.URL}/cards?q=name:${name}`,
+        {
+          headers: {
+            'X-Api-Key': this.config.API_KEY,
+          },
+        },
+      );
+      return result.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
