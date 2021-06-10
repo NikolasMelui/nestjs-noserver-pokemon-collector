80e932ca11b4a8f40d187f74e13cd15c4881ebcd
nikolasmelui <slak@samaradom.ru>
2021-06-10 14:23:57 +0300

    Add TypeORM module in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { InvokerModule } from './invoker/invoker.module';
 
 @Module({
-  imports: [InvokerModule],
+  imports: [
+    InvokerModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: './data/pokemon-collector.sqlite3',
+      entities: [`${__dirname}/**/*.entity{.ts,.js}`],
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
