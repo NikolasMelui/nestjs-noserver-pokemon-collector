134d54b5893b4093b242d33f13c40ad2a99cec86
nikolasmelui <slak@samaradom.ru>
2021-06-10 22:57:18 +0300

    Update InvokerModule imports

--- src/invoker/invoker.module.ts
+++ src/invoker/invoker.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PokemonRepository } from 'src/database/pokemon/pokemon.repository';
-import { DownloaderService } from 'src/services/downloader.service';
+import { PokeapiService } from 'src/services/pokeapi.service';
 import { ParserService } from 'src/services/parser.service';
 import { ReaderService } from 'src/services/reader.service';
 import { WriterService } from 'src/services/writer.service';
 import { InvokerService } from './invoker.service';
+import { PokemonTcgService } from 'src/services/pokemontcg.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([PokemonRepository])],
@@ -14,8 +15,9 @@ import { InvokerService } from './invoker.service';
     InvokerService,
     ReaderService,
     WriterService,
-    DownloaderService,
     ParserService,
+    PokeapiService,
+    PokemonTcgService,
   ],
 })
 export class InvokerModule {}
